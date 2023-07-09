de396ffc9755a8fc3289412168a1539fc506a3f0
alde-oli <alxunit01@gmail.com>
2023-07-09 14:20:17 +0100

    tentative ft_strlcat

--- fonctions/ft_strlcat.c
+++ fonctions/ft_strlcat.c
@@ -1,6 +1,18 @@
 #include "libft.h"
 
-size_t  strlcat(char *dest, const char *src, size_t n)
+size_t  ft_strlcat(char *dest, const char *src, size_t n)
 {
+    int dest_len;
+    int src_len;
+    int new_len;
+    int i;
 
+    dest_len = ft_strlen(dest);
+    src_len = ft_strlen(src);
+    if (src_len < n)
+        new_len = dest_len + src_len;
+    else
+        new_len = dest_len + n;
+    ft_strncat(dest, src, n);
+    return ((size_t)(dest_len + src_len + 1));
 }
