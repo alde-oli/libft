e578bd4bcbc2e2ad823c1f66fad83454727dfc3d
alde-oli <alxunit01@gmail.com>
2023-07-10 13:44:38 +0100

    libc fini

--- ft libc/libft.h
+++ ft libc/libft.h
@@ -28,6 +28,10 @@ int	    ft_strncmp(char *s1, char*s2, unsigned int nb);
 int	    ft_atoi(char *str);
 int     ft_isalpha(int c);
 int     ft_isdigit(int c);
-int     ft_isalnum(int c)
+int     ft_isalnum(int c);
+int     ft_isascii(int c);
+int     ft_isprint(int c);
+int     ft_toupper(int c);
+int     ft_tolower(int c);
 
 #endif
