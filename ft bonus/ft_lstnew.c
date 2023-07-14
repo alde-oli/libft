4aae806f7e5a78c9cb9bf2f6b0dc80e30d1cc412
alde-oli <alxunit01@gmail.com>
2023-07-14 16:04:04 +0100

    + lstnew fini

---
+++ ft bonus/ft_lstnew.c
@@ -0,0 +1,27 @@
+#include "../libft.h"
+
+t_list *ft_lstnew(void const *content, size_t content_size)
+{
+    t_list  *lst;
+
+    lst = (t_list *) malloc(sizeof(t_list));
+    if (!lst)
+        return (NULL);
+    lst->next = NULL;
+    if (content)
+    {
+		lst->content = ft_memalloc(content_size);
+		if (!(lst->content))
+		{
+			free(lst);
+			return (NULL);
+		}
+		ft_memcpy(lst->content, content, lst->content_size = content_size);
+    }
+    else
+    {
+        lst->content = NULL;
+        lst->content_size = NULL;
+    }
+    return (lst);
+}
