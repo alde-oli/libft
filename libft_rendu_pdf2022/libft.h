aeb99b9b5ce31f20f47b221c6501a599b69ab380
alde-oli <alxunit01@gmail.com>
2023-09-20 13:58:41 +0200

    modif param ft_strdup

--- libft_rendu_pdf2022/libft.h
+++ libft_rendu_pdf2022/libft.h
@@ -34,7 +34,7 @@ int     ft_memcmp(const void *s1, const void *s2, size_t n);
 char    *ft_strnstr(const char *h, const char *n, size_t len);
 int	    ft_atoi(char *str);
 void    *ft_calloc(size_t n, size_t size);
-char	*ft_strdup(char *src);
+char	*ft_strdup(const char *src);
 //fonctions supp
 char    *ft_substr(char const *s, unsigned int start, size_t len);
 char    *ft_strjoin(char const *s1, char const *s2);
